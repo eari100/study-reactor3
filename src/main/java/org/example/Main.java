9478f1dfdedd63f8a7dfb6e39fff9eba808d7a17
Jaewook Choi <eari100@naver.com>
2023-08-05 20:42:13 +0900

    Create a Flux or Mono

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a Flux or Mono
+        Flux<String> seq1 = Flux.just("foo", "bar", "foobar");
+
+        List<String> iterable = Arrays.asList("foo", "bar", "foobar");
+        Flux<String> seq2 = Flux.fromIterable(iterable);
+
+        Mono<String> noData = Mono.empty();
+        Mono<String> data = Mono.just("foo");
+        Flux<Integer> numbersFromFiveToSeven = Flux.range(5, 3);
+
+        numbersFromFiveToSeven.subscribe(value -> System.out.println(value));
     }
 }
